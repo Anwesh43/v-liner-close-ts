6b81e9fd69269e1224d3ca35f8f784ca4ddb28e4
Anwesh43 <anweshthecool0@gmail.com>
2020-12-29 12:53:53 +0530

    adding VLinerClose class

--- index.ts
+++ index.ts
@@ -191,3 +191,26 @@ class VLCNode {
         return this 
     }
 }
+
+class VLinerClose {
+
+    curr : VLCNode = new VLCNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
